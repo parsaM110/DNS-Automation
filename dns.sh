7506ff12937494f3a1404b12ca7c2c7517c331b6
parsaM110 <101204725+parsaM110@users.noreply.github.com>
2024-02-16 12:34:11 +0330

    adding color to echo

--- dns.sh
+++ dns.sh
@@ -3,15 +3,15 @@
 # Check the first argument passed to the script
 case "$1" in
     "shecan")
-        echo "shecan"
+	echo -e "\033[0;32mshecan\033[0m"
 	networksetup -setdnsservers Wi-Fi 178.22.122.100 185.51.200.2
         ;;
     "403")
-        echo "403"
+	echo -e "\033[0;34m403\033[0m"
 	networksetup -setdnsservers Wi-Fi 10.202.10.202 10.202.10.102
         ;;
     "clear")
-	echo "clear"
+	echo -e "\033[0;31mclear\033[0m"
 	networksetup -setdnsservers Wi-Fi empty
 	;;
     *)
