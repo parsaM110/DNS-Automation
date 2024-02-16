1826301902066643bcc28c039105e12ed149b2bd
parsaM110 <101204725+parsaM110@users.noreply.github.com>
2024-02-16 12:33:52 +0330

    solving error of two dns setup

--- dns.sh
+++ dns.sh
@@ -2,21 +2,19 @@
 
 # Check the first argument passed to the script
 case "$1" in
-    "403")
-        echo "403"
-	networksetup -setdnsservers Wi-Fi 178.22.122.100
-	networksetup -setdnsservers Wi-Fi 178.22.122.100
-        ;;
     "shecan")
         echo "shecan"
-	networksetup -setdnsservers Wi-Fi 10.202.10.202
-	networksetup -setdnsservers Wi-Fi 10.202.10.102
+	networksetup -setdnsservers Wi-Fi 178.22.122.100 185.51.200.2
+        ;;
+    "403")
+        echo "403"
+	networksetup -setdnsservers Wi-Fi 10.202.10.202 10.202.10.102
         ;;
     "clear")
 	echo "clear"
 	networksetup -setdnsservers Wi-Fi empty
 	;;
     *)
-        echo "Invalid argument. Usage: dns.sh [403|shecan]"
+        echo "Invalid argument. Usage: dns.sh [403|shecan|shecan]"
         ;;
 esac
