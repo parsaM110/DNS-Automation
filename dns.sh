16add4ec8d08f208d09af9db559cf7057df23bfe
parsaM110 <101204725+parsaM110@users.noreply.github.com>
2024-02-16 12:16:54 +0330

    adding dns details | not working shouldn't put it like this use , instead

--- dns.sh
+++ dns.sh
@@ -4,12 +4,17 @@
 case "$1" in
     "403")
         echo "403"
+	networksetup -setdnsservers Wi-Fi 178.22.122.100
+	networksetup -setdnsservers Wi-Fi 178.22.122.100
         ;;
     "shecan")
         echo "shecan"
+	networksetup -setdnsservers Wi-Fi 10.202.10.202
+	networksetup -setdnsservers Wi-Fi 10.202.10.102
         ;;
     "clear")
 	echo "clear"
+	networksetup -setdnsservers Wi-Fi empty
 	;;
     *)
         echo "Invalid argument. Usage: dns.sh [403|shecan]"
