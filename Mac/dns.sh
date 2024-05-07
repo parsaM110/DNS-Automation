52ed1a25bc36c2820e6a4fb463b1f596cc10fe2e
parsaM110 <101204725+parsaM110@users.noreply.github.com>
2024-05-07 20:35:17 +0330

    add dns flush after changing the dns

--- Mac/dns.sh
+++ Mac/dns.sh
@@ -5,14 +5,17 @@ case "$1" in
     "shecan")
 	echo -e "\033[0;32mshecan\033[0m"
 	networksetup -setdnsservers Wi-Fi 178.22.122.100 185.51.200.2
+	sudo dscacheutil -flushcache
         ;;
     "403")
 	echo -e "\033[0;34m403\033[0m"
 	networksetup -setdnsservers Wi-Fi 10.202.10.202 10.202.10.102
+	sudo dscacheutil -flushcache
         ;;
     "clear")
 	echo -e "\033[0;31mclear\033[0m"
 	networksetup -setdnsservers Wi-Fi empty
+	sudo dscacheutil -flushcache
 	;;
     *)
         echo "Invalid argument. Usage: dns.sh [403|shecan|shecan]"
