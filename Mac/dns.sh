734dc09b16131659c6cf376e237fc402bb716232
parsaM110 <101204725+parsaM110@users.noreply.github.com>
2024-08-02 10:34:12 +0330

    Update dns.sh

--- Mac/dns.sh
+++ Mac/dns.sh
@@ -2,7 +2,7 @@
 
 # Check the first argument passed to the script
 case "$1" in
-    "shecan")
+    "shekan")
 	echo -e "\033[0;32mshecan\033[0m"
 	networksetup -setdnsservers Wi-Fi 178.22.122.100 185.51.200.2
 	sudo dscacheutil -flushcache
@@ -10,6 +10,16 @@ case "$1" in
     "403")
 	echo -e "\033[0;34m403\033[0m"
 	networksetup -setdnsservers Wi-Fi 10.202.10.202 10.202.10.102
+	sudo dscacheutil -flushcache
+        ;;
+    "begzar")
+	echo -e "\033[0;36mbegzar\033[0m"
+	networksetup -setdnsservers Wi-Fi 185.55.226.26 185.55.225.25
+	sudo dscacheutil -flushcache
+        ;;
+    "electro")
+	echo -e "\033[0;95melectro\033[0m"
+	networksetup -setdnsservers Wi-Fi 78.157.42.101 78.157.42.100
 	sudo dscacheutil -flushcache
         ;;
     "clear")
