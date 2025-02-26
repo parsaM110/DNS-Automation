65573e34c52c5d79244c69d3eb7bf40ffc243386
parsaM110 <101204725+parsaM110@users.noreply.github.com>
2025-02-26 12:32:16 +0330

    Update dns.ps1

--- Windows/dns.ps1
+++ Windows/dns.ps1
@@ -6,18 +6,22 @@ switch ($arg) {
     "google" {
         Write-Host "google" -ForegroundColor Magenta
         Set-DnsClientServerAddress -InterfaceAlias "Wi-Fi" -ServerAddresses ("8.8.8.8", "8.8.4.4")
+        ipconfig /flushdns
     }
-    "shekan" {
-        Write-Host "shecan" -ForegroundColor Green
+    "shecan" {
+        Write-Host "shekan" -ForegroundColor Green
         Set-DnsClientServerAddress -InterfaceAlias "Wi-Fi" -ServerAddresses ("178.22.122.100", "185.51.200.2")
+        ipconfig /flushdns
     }
     "403" {
         Write-Host "403" -ForegroundColor Blue
         Set-DnsClientServerAddress -InterfaceAlias "Wi-Fi" -ServerAddresses ("10.202.10.202", "10.202.10.102")
+        ipconfig /flushdns
     }
     "clear" {
-   	 Write-Host "clear" -ForegroundColor Red
+   	    Write-Host "clear" -ForegroundColor Red
     	Set-DnsClientServerAddress -InterfaceAlias "Wi-Fi" -ResetServerAddresses
+	    ipconfig /flushdns
     }
     default {
         Write-Host "Invalid argument. Usage: dns.ps1 [403|shekan|google|clear]"
